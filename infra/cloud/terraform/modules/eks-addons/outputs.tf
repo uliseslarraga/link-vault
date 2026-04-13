1771e05746e1a5ade7c346b2769ec67baabe87fd
Ulises Larraga <uliseslarraga@gmail.com>
2026-04-13 15:19:14 -0600

    fix(infra/addons): update outputs.tf after basic → daemonset rename
    aws_eks_addon.basic was renamed to aws_eks_addon.daemonset in the
    previous commit but outputs.tf still referenced the old name.
    Also adds coredns to the addon_ids output.

--- infra/cloud/terraform/modules/eks-addons/outputs.tf
+++ infra/cloud/terraform/modules/eks-addons/outputs.tf
@@ -1,7 +1,8 @@
 output "addon_ids" {
   description = "Map of all addon IDs keyed by addon name"
   value = merge(
-    { for k, v in aws_eks_addon.basic : k => v.id },
+    { for k, v in aws_eks_addon.daemonset : k => v.id },
+    { "coredns" = aws_eks_addon.coredns.id },
     { "aws-ebs-csi-driver" = aws_eks_addon.ebs_csi.id },
   )
 }
